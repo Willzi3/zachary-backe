a23be44c4b8d614a94984bf9880bae0a736ce9bc
Willzi3 <williamszachary787@gmail.com>
2022-09-20 18:09:46 +0200

    update products route

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -38,12 +38,10 @@ router.get("/:id", (req, res) => {
     // the below allows you to only need one const, but every input required is inside of the brackets
     const {
         name,
-        price,
         descriptions,
         image,
         category,
         created_date,
-        stock
     } = req.body;
     // OR
     // the below requires you to add everything one by one
@@ -51,7 +49,7 @@ router.get("/:id", (req, res) => {
     try {
       con.query(
         //When using the ${}, the content of con.query MUST be in the back tick
-        `UPDATE products set name="${name}", price="${price}", descriptions="${descriptions}", image="${image}", category="${category}", created_date="${created_date}", stock="${stock}" WHERE product_id ="${req.params.id}"`,
+        `UPDATE products set name="${name}", descriptions="${descriptions}", image="${image}", category="${category}", created_date="${created_date}" WHERE product_id ="${req.params.id}"`,
         (err, result) => {
           if (err) throw err;
           res.send(result);
@@ -73,12 +71,10 @@ router.get("/:id", (req, res) => {
     // the below allows you to only need one const, but every input required is inside of the brackets
     const {
       name,
-      price,
       descriptions,
       image,
       category,
       created_date,
-      stock
     } = req.body;
     // OR
     // the below requires you to add everything one by one
@@ -88,12 +84,11 @@ router.get("/:id", (req, res) => {
         //When using the ${}, the content of con.query MUST be in the back tick
         `INSERT INTO products (
             name,
-            price,
             descriptions,
             image,
             category,
             created_date,
-            stock) VALUES ( "${name}", "${price}", "${descriptions}", "${image}", "${category}", "${created_date}", "${stock}" )`,
+          ) VALUES ( "${name}", "${descriptions}", "${image}", "${category}", "${created_date}" )`,
         (err, result) => {
           if (err) throw err;
           console.log("product successfully created")
@@ -104,24 +99,8 @@ router.get("/:id", (req, res) => {
       console.log(error);
       res.status(400).send(error);
     }}
-    // else{
-  //     res.send("Not an Admin, access denied!");
-  //   } 
  );
   
-  // Delete one products
-  // router.delete("/:id", (req, res) => {
-  //   try {
-  //     con.query(
-  //       `DELETE FROM products WHERE id=${req.params.id}`,
-  //       (err, result) => {
-  //         if (err) throw err;
-  //         res.send(result);
-  //       }
-  //     );
-  //   } catch (error) {
-  //     console.log(error);
-  //   })
 
 
     router.delete("/:id", (req, res) => {
